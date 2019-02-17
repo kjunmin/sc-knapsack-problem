d8cc830a2704a273bcbe816ba4425aa8143a9f92
mephet <matthew_kohjm@hotmail.sg>
2019-02-17 18:24:03 +0800

    Create Knapsack.java

---
+++ Knapsack.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int solve(int[] items, int index, int sum, int cap) {
+		System.out.println("index:"+index+" sum:"+sum+" cap:"+cap);
+		if (index > items.length - 1 || cap == 0) {
+			System.out.println("Returning sum " + sum);
+			return sum;
+		}
+		
+		if (items[index] > cap) {
+			return solve(items, index + 1, sum, cap);
+		} else {
+			System.out.println("Picked " + items[index]);
+			int picked = solve(items, index+1, sum+1, cap - items[index]);
+			System.out.println("Excluded " + items[index]);
+			
+			int excluded = 0;
+			//if item was picked already previously, decrease the sum to prevent double counting
+			if (sum > 0) {
+				excluded = solve(items, index+1, sum - 1 , cap);
+			} else {
+				excluded = solve(items, index+1, sum, cap);
+			}
+			
+			return excluded + picked;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.print("TEST");
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int[] items = new int[num];
+		for (int i = 0; i < num; i++) {
+			items[i] = sc.nextInt();
+		}
+		int cap = sc.nextInt();
+		
+		System.out.println(solve(items, 0, 0, cap) + 1);
+	}
+
+}
